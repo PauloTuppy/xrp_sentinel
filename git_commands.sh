fb1a5a5dbaed1f5be316046ae42a40dab8beca44
PauloTuppy <paulotuppyjatoba@gmail.com>
2025-05-18 21:46:57 -0300

    Latest updates - push for show

---
+++ git_commands.sh
@@ -0,0 +1,7 @@
+# Create and push to GitHub repository using GitHub CLI
+gh repo create PauloTuppy/xrp_sentinel --public --source=. --push
+
+# Push with a descriptive message for visibility
+git add .
+git commit -m "Latest updates - push for show"
+git push
